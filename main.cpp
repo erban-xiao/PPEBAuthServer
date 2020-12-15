0fd22b22337afaa4b3360c4560f3d933b6f13747
erban <943186182@qq.com>
2020-12-15 22:25:17 +0800

    提交

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by LQYHE on 2020/12/15.
+//
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout<<"argc : "<<argc << std::endl;
+    for(int i = 0; i< argc;++i)
+    {
+        std::cout<<"argv["<<i<<"] : "<<argv[i]<<std::endl;
+    }
+
+    return 0;
+}
