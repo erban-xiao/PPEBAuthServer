82d9589b94ccc438b6323c9cde2bf7470a4dc0c7
erban <943186182@qq.com>
2020-12-16 02:11:49 +0800

    日常更新

---
+++ authenservice.h
@@ -0,0 +1,14 @@
+//
+// Created by LQYHE on 2020/12/16.
+//
+
+#ifndef EB_AUTHEN_SERVER__AUTHENSERVICE_H
+#define EB_AUTHEN_SERVER__AUTHENSERVICE_H
+#include "ebresult.h"
+class AuthenService
+{
+public:
+	eb_tools::EBResult<> Start();
+};
+
+#endif //EB_AUTHEN_SERVER__AUTHENSERVICE_H
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <iostream>
+#include "authenservice.h"
 
 int main(int argc, char* argv[])
 {
@@ -12,5 +13,7 @@ int main(int argc, char* argv[])
         std::cout<<"argv["<<i<<"] : "<<argv[i]<<std::endl;
     }
 
+	AuthenService authenService;
+    authenService.Start();
     return 0;
 }
